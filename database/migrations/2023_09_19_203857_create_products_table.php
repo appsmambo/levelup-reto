865e55dd54f096b2e9fabc918f489ea3de1ff538
Juan Quintanilla <juan.quintanilla@interseguro.com.pe>
2023-09-19 16:23:21 -0500

    campos de la tabla products

--- database/migrations/2023_09_19_203857_create_products_table.php
+++ database/migrations/2023_09_19_203857_create_products_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price', $precision = 8, $scale = 2);
             $table->timestamps();
         });
     }
